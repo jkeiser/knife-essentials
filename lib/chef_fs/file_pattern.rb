168a64cd0e81d5e076b959132858ce875c8d836b
jkeiser <jkeiser@opscode.com>
2012-04-19 00:29:41 -0700

    Add rdoc for ChefFS::FilePattern

--- lib/chef_fs/file_pattern.rb
+++ lib/chef_fs/file_pattern.rb
@@ -2,13 +2,46 @@ require 'chef_fs'
 require 'chef_fs/path_utils'
 
 module ChefFS
+  #
+  # Represents a glob pattern.  This class is designed so that it can
+  # match arbitrary strings, and tell you about partial matches.
+  #
+  # Examples:
+  # * <tt>a*z</tt>
+  #   - Matches <tt>abcz</tt>
+  #   - Does not match <tt>ab/cd/ez</tt>
+  #   - Does not match <tt>xabcz</tt>
+  # * <tt>a**z</tt>
+  #   - Matches <tt>abcz</tt>
+  #   - Matches <tt>ab/cd/ez</tt>
+  #
+  # Special characters supported:
+  # * <tt>/</tt> (and <tt>\\</tt> on Windows) - directory separators
+  # * <tt>\*</tt> - match zero or more characters (but not directory separators)
+  # * <tt>\*\*</tt> - match zero or more characters, including directory separators
+  # * <tt>?</tt> - match exactly one character (not a directory separator)
+  # Only on Unix:
+  # * <tt>[abc0-9]</tt> - match one of the included characters
+  # * <tt>\\<character></tt> - escape character: match the given character
+  #
   class FilePattern
+    # Initialize a new FilePattern with the pattern string.
     def initialize(pattern)
       @pattern = pattern
     end
 
+    # The pattern string.
     attr_reader :pattern
 
+    # Reports whether this pattern could match children of <tt>path</tt>.
+    # If the pattern doesn't match the path up to this point or
+    # if it matches and doesn't allow further children, this will
+    # return <tt>false</tt>.
+    #
+    #   abc/def.could_match_children?('abc') == true
+    #   abc.could_match_children?('abc') == false
+    #   abc/def.could_match_children?('x') == false
+    #   a**z.could_match_children?('ab/cd') == true
     def could_match_children?(path)
       path_parts = ChefFS::PathUtils::split(path)
       # If the pattern is shorter than the path (or same size), children will be larger than the pattern, and will not match.
@@ -21,25 +54,53 @@ module ChefFS
       return true
     end
 
+    # Returns the next child name in an exact path.
+    #
+    # If this pattern can only match one possible child of the
+    # given <tt>path</tt>, this method returns its name.
+    #
+    #   abc/def.exact_child_name_under('abc') == 'def'
+    #   abc/def/ghi.exact_child_name_under('abc') == 'def'
+    #   abc/*/ghi.exact_child_name_under('abc') == nil
+    #   abc/*/ghi.exact_child_name_under('abc/def') == 'ghi'
+    #   abc/**/ghi.exact_child_name_under('abc/def') == nil
+    # 
+    # This method assumes <tt>could_match_children?(path)</tt> is <tt>true</tt>.
     def exact_child_name_under(path)
       dirs_in_path = ChefFS::PathUtils::split(path).length
       return nil if exact_parts.length <= dirs_in_path
       return exact_parts[dirs_in_path]
     end
 
+    # If this pattern represents an exact path, returns the exact path.
+    #
+    #   abc/def.exact_path == 'abc/def'
+    #   abc/*def.exact_path == 'abc/def'
+    #   abc/x\\yz.exact_path == 'abc/xyz'
     def exact_path
       return nil if has_double_star || exact_parts.any? { |part| part.nil? }
       ChefFS::PathUtils::join(*exact_parts)
     end
 
+    # Returns <tt>true+ if this pattern matches the path, <tt>false+ otherwise.
+    #
+    #   abc/*/def.match?('abc/foo/def') == true
+    #   abc/*/def.match?('abc/foo') == false
     def match?(path)
       !!regexp.match(path)
     end
 
+    # Returns the string pattern
     def to_s
       pattern
     end
 
+    # Given a relative file pattern and a directory, makes a new file pattern
+    # starting with the directory.
+    #
+    #   FilePattern.relative_to('/usr/local', 'bin/*grok') == FilePattern.new('/usr/local/bin/*grok')
+    #
+    # BUG: this does not support patterns starting with <tt>..</tt>
     def self.relative_to(dir, pattern)
       return FilePattern.new(pattern) if pattern =~ /^#{ChefFS::PathUtils::regexp_path_separator}/
       FilePattern.new(ChefFS::PathUtils::join(dir, pattern))
@@ -142,7 +203,7 @@ module ChefFS
               exact = nil
               regexp << part
             else
-              exact += part if !exact.nil?
+              exact <tt>= part if !exact.nil?
               regexp << "\\#{part}"
             end
           end
