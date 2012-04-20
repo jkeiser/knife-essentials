5a27f2ee3523ef1431bc8a6d99d3c18731884739
jkeiser <jkeiser@opscode.com>
2012-04-20 00:32:28 -0700

    Make NonexistentFSObject raise NotFound on read

--- lib/chef_fs/file_system/nonexistent_fs_object.rb
+++ lib/chef_fs/file_system/nonexistent_fs_object.rb
@@ -14,6 +14,10 @@ module ChefFS
       def child(name)
         NonexistentFSObject.new(name, self)
       end
+
+      def read
+        raise ChefFS::FileSystem::NotFoundException, "Nonexistent object"
+      end
     end
   end
 end
--- lib/chef_fs/file_system/not_found_exception.rb
+++ lib/chef_fs/file_system/not_found_exception.rb
@@ -2,7 +2,7 @@ module ChefFS
   module FileSystem
     class NotFoundException < Exception
       def initialize(other_exception)
-        super(other_exception.message)
+        super(other_exception.respond_to?(:message) ? other_exception.message : message)
         @exception = other_exception
       end
 
