5fd0f98b503168167dd26156e21a02265f79d19f
jkeiser <jkeiser@opscode.com>
2012-04-26 19:36:46 -0700

    Fixes #32: make diff ignore cookbook files that won't be uploaded

--- lib/chef_fs/file_system/cookbook_dir.rb
+++ lib/chef_fs/file_system/cookbook_dir.rb
@@ -13,6 +13,18 @@ module ChefFS
 
       attr_reader :versions
 
+      COOKBOOK_SEGMENT_INFO = {
+        :attributes => { :ruby_only => true },
+        :definitions => { :ruby_only => true },
+        :recipes => { :ruby_only => true },
+        :libraries => { :ruby_only => true },
+        :templates => { :recursive => true },
+        :files => { :recursive => true },
+        :resources => { :ruby_only => true, :recursive => true },
+        :providers => { :ruby_only => true, :recursive => true },
+        :root_files => { }
+      }
+
       def add_child(child)
         @children << child
       end
@@ -25,11 +37,23 @@ module ChefFS
         children.select { |child| child.name == name }.first || NonexistentFSObject.new(name, self)
       end
 
+      def can_have_child?(name, is_dir)
+        # A cookbook may have files in its root directory, and directories matching the known segment dirs
+        if name != 'root_files' && Chef::CookbookVersion::COOKBOOK_SEGMENTS.any? { |segment| segment.to_s == name }
+          return is_dir
+        end
+        !is_dir
+      end
+
       def children
         if @children.nil?
           @children = []
           Chef::CookbookVersion::COOKBOOK_SEGMENTS.each do |segment|
             next unless manifest.has_key?(segment)
+            segment_info = COOKBOOK_SEGMENT_INFO[segment]
+
+            # Go through each file in the manifest for the segment, and
+            # add cookbook subdirs and files for it.
             manifest[segment].each do |segment_file|
               parts = segment_file['path'].split('/')
               # Get or create the path to the file
@@ -38,7 +62,7 @@ module ChefFS
                 old_container = container
                 container = old_container.children.select { |child| part == child.name }.first
                 if !container
-                  container = CookbookSubdir.new(part, old_container)
+                  container = CookbookSubdir.new(part, old_container, segment_info[:ruby_only], segment_info[:recursive])
                   old_container.add_child(container)
                 end
               end
--- lib/chef_fs/file_system/cookbook_subdir.rb
+++ lib/chef_fs/file_system/cookbook_subdir.rb
@@ -5,9 +5,11 @@ require 'chef_fs/file_system/base_fs_dir'
 module ChefFS
   module FileSystem
     class CookbookSubdir < BaseFSDir
-      def initialize(name, parent)
+      def initialize(name, parent, ruby_only, recursive)
         super(name, parent)
         @children = []
+        @ruby_only = ruby_only
+        @recursive = recursive
       end
 
       attr_reader :versions
@@ -15,6 +17,15 @@ module ChefFS
 
       def add_child(child)
         @children << child
+      end
+
+      def can_have_child?(name, is_dir)
+        if is_dir
+          return false if !@recursive
+        else
+          return false if @ruby_only && name !~ /\.rb$/
+        end
+        true
       end
 
       def rest
