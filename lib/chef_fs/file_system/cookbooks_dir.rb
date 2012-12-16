5883ba0dbacff2e960ec03d4f7a9d18d92e6da29
jkeiser <jkeiser@opscode.com>
2012-12-15 17:14:15 -0800

    Add license

--- lib/chef_fs/file_system/cookbooks_dir.rb
+++ lib/chef_fs/file_system/cookbooks_dir.rb
@@ -1,3 +1,21 @@
+#
+# Author:: John Keiser (<jkeiser@opscode.com>)
+# Copyright:: Copyright (c) 2012 Opscode, Inc.
+# License:: Apache License, Version 2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 require 'chef_fs/file_system/rest_list_dir'
 require 'chef_fs/file_system/cookbook_dir'
 
@@ -29,10 +47,8 @@ module ChefFS
           uploader = Chef::CookbookUploader.new(other_cookbook_version, other.parent.file_path)
           # Chef 11 changes this API
           if uploader.respond_to?(:upload_cookbook)
-            puts "upload_cookbook"
             uploader.upload_cookbook
           else
-            puts "upload_cookbooks"
             uploader.upload_cookbooks
           end
         rescue Net::HTTPServerException => e
