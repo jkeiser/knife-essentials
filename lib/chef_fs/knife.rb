b8fba6bebf4e226d6eafc0030c10450b70e6eea6
jkeiser <jkeiser@opscode.com>
2013-01-05 09:06:47 -0800

    Fix bug where it thought you were in "cookbooks1" when you were really in "cookbooks123"

--- lib/chef_fs/knife.rb
+++ lib/chef_fs/knife.rb
@@ -118,7 +118,9 @@ module ChefFS
       object_paths.each_pair do |name, paths|
         paths.each do |path|
           realest_path = ChefFS::PathUtils.realest_path(path)
-          if absolute_path[0,realest_path.length] == realest_path
+          if absolute_path[0,realest_path.length] == realest_path &&
+            (absolute_path.length == realest_path.length ||
+              absolute_path[realest_path.length] =~ /#{PathUtils.regexp_path_separator}/)
             relative_path = ChefFS::PathUtils::relative_to(absolute_path, realest_path)
             return relative_path == '.' ? "/#{name}" : "/#{name}/#{relative_path}"
           end
