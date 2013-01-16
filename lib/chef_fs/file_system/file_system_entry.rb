483d90baf19b5bbdf8a9ad62f095827d4d107edf
jkeiser <jkeiser@opscode.com>
2013-01-16 09:03:15 -0800

    Add "must delete recursively" error to knife delete local

--- lib/chef_fs/file_system/file_system_entry.rb
+++ lib/chef_fs/file_system/file_system_entry.rb
@@ -19,6 +19,7 @@
 require 'chef_fs/file_system/base_fs_dir'
 require 'chef_fs/file_system/rest_list_dir'
 require 'chef_fs/file_system/not_found_error'
+require 'chef_fs/file_system/must_delete_recursively_error'
 require 'chef_fs/path_utils'
 require 'fileutils'
 
@@ -63,7 +64,11 @@ module ChefFS
           if recurse
             FileUtils.rm_rf(file_path)
           else
-            File.rmdir(file_path)
+            begin
+              Dir.rmdir(file_path)
+            rescue Errno::ENOTEMPTY
+              raise MustDeleteRecursivelyError.new(self, $!)
+            end
           end
         else
           File.delete(file_path)
