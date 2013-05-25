5a2b1bdcd9352ba212c1b4443472ff211d81e560
John Keiser <jkeiser@opscode.com>
2013-05-24 23:14:31 -0700

    Add cookbook support to knife zero

---
+++ lib/chef_fs/file_system/memory_root.rb
@@ -0,0 +1,19 @@
+require 'chef_fs/file_system/memory_dir'
+
+module ChefFS
+  module FileSystem
+    class MemoryRoot < MemoryDir
+      def initialize(pretty_name, cannot_be_in_regex = nil)
+        super('', nil)
+        @pretty_name = pretty_name
+        @cannot_be_in_regex = cannot_be_in_regex
+      end
+
+      attr_reader :cannot_be_in_regex
+
+      def path_for_printing
+        @pretty_name
+      end
+    end
+  end
+end
