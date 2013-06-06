08a46e1b6838ddd208b97d42e4f868409d95a935
John Keiser <jkeiser@opscode.com>
2013-06-06 11:02:52 -0700

    Make file system description prettier

--- lib/chef_fs/file_system/chef_repository_file_system_root_dir.rb
+++ lib/chef_fs/file_system/chef_repository_file_system_root_dir.rb
@@ -72,13 +72,13 @@ module ChefFS
         repo_path = File.dirname(child_paths['cookbooks'][0])
         result = "repository at #{repo_path}\n"
         if Chef::Config[:versioned_cookbooks]
-          result << "  - Multiple versions per cookbook\n"
+          result << "  Multiple versions per cookbook\n"
         else
-          result << "  - One version per cookbook\n"
+          result << "  One version per cookbook\n"
         end
         child_paths.each_pair do |name, paths|
           if paths.any? { |path| File.dirname(path) != repo_path }
-            result << "  - #{name} at #{paths.map { }.join('')}\n"
+            result << "  #{name} at #{paths.join(', ')}\n"
           end
         end
         result
