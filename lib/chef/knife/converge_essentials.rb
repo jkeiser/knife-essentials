6f76a83b83a67b36f544b85fddb2d3a54d12b8a3
John Keiser <jkeiser@opscode.com>
2013-06-13 09:40:12 -0700

    Add arguments (override runlist) to knife converge

--- lib/chef/knife/converge_essentials.rb
+++ lib/chef/knife/converge_essentials.rb
@@ -8,20 +8,10 @@ class Chef
       banner "knife converge [PATTERN1 ... PATTERNn]"
 
       deps do
-        begin
-          require 'chef_zero/server'
-        rescue LoadError
-          STDERR.puts <<EOM
-ERROR: chef-zero must be installed to run "knife serve"!  To install:
-
-    gem install chef-zero
-
-EOM
-          exit(1)
-        end
-        require 'chef_fs/chef_fs_data_store'
+        require 'chef'
         require 'chef/log'
         require 'chef/application/client'
+        require 'chef_fs/file_system'
       end
 
       option :port,
@@ -29,9 +19,24 @@ EOM
         :long => '--port=PORT',
         :description => "Port to listen on (default: 8889)"
 
+      attr_accessor :exit_code
+
       def configure_chef
         super
 
+        self.exit_code = 0
+      end
+
+      def run
+        self.exit_code = 0
+        # Figure out the run list based on the passed-in objects
+        run_list = get_run_list(name_args)
+
+        if exit_code != 0
+          exit exit_code
+        end
+
+        # Configure and run chef client
         if config[:config_file]
           dot_chef = File.dirname(config[:config_file])
           if File.basename(dot_chef) == '.chef'
@@ -42,17 +47,52 @@ EOM
             Chef::Config[:file_checksum_path] = File.join(dot_chef, 'checksums') if Chef::Config[:file_checksum_cache] == "/var/chef/checksums"
           end
         end
-      end
-
-      def run
-        run_chef_client
-      end
-
-      def run_chef_client
         client = Chef::Application::Client.new
+        if run_list != ''
+          client.config[:override_runlist] = run_list
+        end
         client.configure_logging # Bypass configure_chef, which we've already done adequately.
         client.setup_application
         client.run_application
+      end
+
+      def get_run_list(args)
+        result = []
+        args.each do |arg|
+          if arg.start_with?('recipe[') || arg.start_with?('role[')
+            result += arg.split(',')
+
+          else
+            begin
+              ChefFS::FileSystem.list(chef_fs, pattern_arg_from(arg)).each do |entry|
+                if entry.parent && entry.parent.path == '/cookbooks'
+                  result << "recipe[#{entry.name}]"
+
+                elsif entry.parent && entry.parent.name == 'recipes' &&
+                      entry.parent.parent && entry.parent.parent.parent && entry.parent.parent.parent.name == 'cookbooks' &&
+                      entry.name[-3..-1] == '.rb'
+                  cookbook_name = entry.parent.parent.name
+                  recipe_name = entry.name[0..-4]
+                  result << "recipe[#{cookbook_name}::#{recipe_name}]"
+
+                elsif entry.parent && entry.parent.name == 'roles'
+                  result << "role[#{entry.name}]"
+
+                else
+                  ui.error "arguments must be cookbooks, recipes or roles!  #{format_path{e.entry}} is not a cookbook, recipe or role."
+                  self.exit_code = 1
+                end
+              end
+            rescue ChefFS::FileSystem::OperationFailedError => e
+              "#{format_path_with_root(e.entry)} #{e.reason}."
+              self.exit_code = 1
+            rescue ChefFS::FileSystem::NotFoundError => e
+              ui.error "#{format_path(e.entry)}: No such file or directory"
+              self.exit_code = 1
+            end
+          end
+        end
+        result.join(',')
       end
     end
   end
